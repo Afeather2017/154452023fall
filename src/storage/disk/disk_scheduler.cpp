7d6822842ac705d2416eb33351b3876414c0d555
afeather clong <afeather@foxmail.com>
2024-02-18 22:12:46 +0800

    Project1,task2 finished

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -11,17 +11,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "storage/disk/disk_scheduler.h"
+#include <optional>
 #include "common/exception.h"
 #include "storage/disk/disk_manager.h"
 
 namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
-  // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
-
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
 }
@@ -34,8 +30,23 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
+  request_queue_.Put(std::move(r));
+}
 
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  for (;;) {
+    std::optional r{request_queue_.Get()};
+    if (!r.has_value()) {
+      return;
+    }
+    if (r.value().is_write_) {
+      disk_manager_->WritePage(r.value().page_id_, r.value().data_);
+    } else {
+      disk_manager_->ReadPage(r.value().page_id_, r.value().data_);
+    }
+    r.value().callback_.set_value(true);
+  }
+}
 
 }  // namespace bustub
