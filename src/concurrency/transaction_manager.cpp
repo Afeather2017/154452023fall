ae638e30fb68d323e73e93cdf07ec80afb1d00dd
afeather clong <afeather@foxmail.com>
2024-04-14 14:58:06 +0800

    Project4,1 completed!

--- src/concurrency/transaction_manager.cpp
+++ src/concurrency/transaction_manager.cpp
@@ -42,7 +42,9 @@ auto TransactionManager::Begin(IsolationLevel isolation_level) -> Transaction * 
   auto *txn_ref = txn.get();
   txn_map_.insert(std::make_pair(txn_id, std::move(txn)));
 
-  // TODO(fall2023): set the timestamps here. Watermark updated below.
+  // set read ts
+  timestamp_t read_ts = last_commit_ts_;
+  txn_ref->read_ts_ = read_ts;
 
   running_txns_.AddTxn(txn_ref->read_ts_);
   return txn_ref;
@@ -72,6 +74,7 @@ auto TransactionManager::Commit(Transaction *txn) -> bool {
   std::unique_lock<std::shared_mutex> lck(txn_map_mutex_);
 
   // TODO(fall2023): set commit timestamp + update last committed timestamp here.
+  txn->commit_ts_ = ++last_commit_ts_;
 
   txn->state_ = TransactionState::COMMITTED;
   running_txns_.UpdateCommitTs(txn->commit_ts_);
--- src/concurrency/watermark.cpp
+++ src/concurrency/watermark.cpp
@@ -8,12 +8,21 @@ auto Watermark::AddTxn(timestamp_t read_ts) -> void {
   if (read_ts < commit_ts_) {
     throw Exception("read ts < commit ts");
   }
-
+  current_reads_[read_ts]++;
   // TODO(fall2023): implement me!
 }
 
 auto Watermark::RemoveTxn(timestamp_t read_ts) -> void {
   // TODO(fall2023): implement me!
+  auto iter = current_reads_.find(read_ts);
+  if (iter == current_reads_.end()) {
+    throw Exception{"Removing invalid timestamp"};
+  }
+  iter->second--;
+  if (iter->second == 0) {
+    current_reads_.erase(iter);
+  }
+  watermark_ = current_reads_.empty() ? commit_ts_: current_reads_.begin()->first;
 }
 
 }  // namespace bustub
